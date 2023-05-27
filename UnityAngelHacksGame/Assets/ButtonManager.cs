9b1cd8caa4146e8fae61ea72cf65e359c9678e59
jjustinjc <63550112+jjustinjc@users.noreply.github.com>
2023-05-27 15:10:24 -0400

    made simple menu

---
+++ UnityAngelHacksGame/Assets/ButtonManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Start
+        Debug.Log("Start");
+        
+        // SceneManager.LoadScene();
+    }
+    public void Settings()
+    {
+        //Start
+        Debug.Log("Settings");
+        
+        // SceneManager.LoadScene();
+    }
+    public void Quit()
+    {
+        //Start
+        Debug.Log("Quit");
+        
+        // SceneManager.LoadScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
