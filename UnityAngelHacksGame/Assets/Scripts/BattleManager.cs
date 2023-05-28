9c1d9b236791ea5111d9153de3bfc98ac7a4f4b9
Raymond Xu <ray01778@gmail.com>
2023-05-28 04:31:45 -0400

    updated mm

--- UnityAngelHacksGame/Assets/Scripts/BattleManager.cs
+++ UnityAngelHacksGame/Assets/Scripts/BattleManager.cs
@@ -5,32 +5,36 @@ using UnityEngine.InputSystem;
 
 public class BattleManager : MonoBehaviour
 {
-    [SerializeField]private ButtonManager bManager;
+    // [SerializeField]private ButtonManager bManager;
     // Start is called before the first frame update
     // private 
-    [SerializeField]private int[] party = new int[3];
+    [SerializeField] private int[] party = new int[3];
     private Character selectedCharacter;
     private int selectedCharacterId, currentCharacterIndex;
     private bool skillActive;
     private Skill selectedSkill;
 
-    public void SelectSkill(Skill action){
+    public void SelectSkill(Skill action)
+    {
         selectedSkill = action;
     }
-    public void SelectCharacter(Character target){
+    public void SelectCharacter(Character target)
+    {
 
         currentCharacterIndex = getSelectedCharacterIndex();
         selectedCharacter = target;
         selectedCharacterId = target.getId();
-        
+
 
     }
-    public int getSelectedCharacterIndex(){
+    public int getSelectedCharacterIndex()
+    {
         for (int i = 0; i < party.GetLength(0); i++)
         {
-            if(party[i]==selectedCharacterId){
+            if (party[i] == selectedCharacterId)
+            {
                 return i;
-                
+
             }
 
         }
@@ -41,19 +45,24 @@ public class BattleManager : MonoBehaviour
 
 
         skillActive = true;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
-            if(skillActive !=true){
-                if(currentCharacterIndex != getSelectedCharacterIndex()){
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (skillActive != true)
+            {
+                if (currentCharacterIndex != getSelectedCharacterIndex())
+                {
                     Debug.Log("switch");
                 }
-            }else if(skillActive == true){
+            }
+            else if (skillActive == true)
+            {
                 Debug.Log("skill");
             }
         }
