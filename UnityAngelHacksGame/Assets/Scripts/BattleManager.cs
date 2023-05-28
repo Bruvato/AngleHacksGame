17c947ed11ac93811b064553e3e418367a9ec1e7
Raymond Xu <ray01778@gmail.com>
2023-05-28 03:59:59 -0400

    mainmenu

--- UnityAngelHacksGame/Assets/Scripts/BattleManager.cs
+++ UnityAngelHacksGame/Assets/Scripts/BattleManager.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    [SerializeField]private ButtonManager bManager;
+    // [SerializeField]private ButtonManager bManager;
     // Start is called before the first frame update
     // private 
     private Character selectedCharacter;
     private Skill selectedSkill;
 
-    public void SelectSkill(Skill action){
+    public void SelectSkill(Skill action)
+    {
         selectedSkill = action;
     }
-    public void SelectCharacter(Character target){
+    public void SelectCharacter(Character target)
+    {
         selectedCharacter = target;
-    
+
     }
     void Awake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ UnityAngelHacksGame/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Start");
+        SceneManager.LoadScene("Map");
+    }
+    public void Settings()
+    {
+        Debug.Log("Settings");
+        SceneManager.LoadScene("Settings");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+}
