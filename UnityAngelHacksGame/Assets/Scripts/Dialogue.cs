b347b65ad713a9e94393ff2fa83607b011038b96
Raymond Xu <ray01778@gmail.com>
2023-06-01 10:35:28 -0400

    updated dialogue system

--- UnityAngelHacksGame/Assets/Scripts/Dialogue.cs
+++ UnityAngelHacksGame/Assets/Scripts/Dialogue.cs
@@ -3,73 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+public class Dialogue
 {
-    [SerializeField] private TextMeshProUGUI textCompenent;
-    [SerializeField] private string[] lines;
-    [SerializeField] private float textSpeed;
+    public string name;
 
-    [SerializeField] private AudioSource src;
-    [SerializeField] private AudioClip sfx;
+    [TextArea(3, 10)]
+    public string[] sentences;
 
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject melonie;
-
-    private int i;
-    // Start is called before the first frame update
-    void Start()
-    {
-        textCompenent.text = string.Empty;
-        StartDialogue();
-        src.clip = sfx;
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (textCompenent.text == lines[i])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textCompenent.text = lines[i];
-            }
-        }
-    }
-
-    private void StartDialogue()
-    {
-        i = 0;
-        StartCoroutine(TypeLine());
-
-    }
-
-    private IEnumerator TypeLine()
-    {
-        foreach (char c in lines[i].ToCharArray())
-        {
-            textCompenent.text += c;
-            src.Play();
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    private void NextLine()
-    {
-        if (i < lines.Length - 1)
-        {
-            i++;
-            textCompenent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            gameObject.SetActive(false);
-            player.SetActive(false);
-        }
-    }
 }
--- UnityAngelHacksGame/Assets/Scripts/DialogueManager.cs
+++ UnityAngelHacksGame/Assets/Scripts/DialogueManager.cs
@@ -1,19 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+    public float textSpeed;
+
+
     private Queue<string> sentences;
-    // Start is called before the first frame update
+    private string sentence;
+
     void Start()
     {
         sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
+
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        // DisplayNextSentence();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentences.Peek()));
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        if (sentences.Peek() == dialogueText.text)
+        {
+            sentences.Dequeue();
+
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentences.Peek()));
+
+        }
+        else
+        {
+            StopAllCoroutines();
+            dialogueText.text = sentences.Peek();
+        }
+
+        // sentence = sentences.Dequeue();
+        // StopAllCoroutines();
+        // StartCoroutine(TypeSentence(sentence));
+
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
 
     }
 }
